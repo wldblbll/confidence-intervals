a7872d09eddc88a3bbf45691b5af27b4b9bbaf30
wldblbll <walidzz@gmail.com>
2020-12-06 22:19:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
-import pandas as p
+#import pandas as p
 import sys
 import datetime
-import plotly.express as px
+#import plotly.express as px
 import streamlit
 import os
-from math import ceil as arrondi_sup
+#from math import ceil as arrondi_sup
+import math
+
 import pandas as p
 
 __version__ = 1.0
@@ -14,21 +16,35 @@ try:
 except:
     pass
 
+def get_proportion_bounds(n, r, z):
+    ##### Calculation
+    p = r/n
+    q = 1-p
+    A = 2*r + z*z
+    B = z*math.sqrt(z*z+4*r*q)
+    C = 2.*(n+z*z)
+    # Confidence Interval Lower Bound
+    cilb = (A-B)/C
+    # Confidence Interval Upper Bound
+    ciup = (A+B)/C
+    return cilb, ciub
+
+
 streamlit.title("Confidence Intervals calculator")
 status = streamlit.empty()
-CommercialLaunchDate = streamlit.sidebar.date_input("CommercialLaunchDate", value=p.to_datetime('01/05/2023', format='%d/%m/%Y'))
-zone = streamlit.sidebar.selectbox("Zone", ["EUR", "E2A", "ADS", "ADN", "CHN", "MSA"])
-DesignType = streamlit.sidebar.selectbox("DesignType", ["CLEAN SHEET", "REFRESH", "EXTENSION"])
-BaliseType = streamlit.sidebar.selectbox("BaliseType", ["B+M", "M"])
-GanttCategory = streamlit.sidebar.selectbox("GanttCategory", ["WINTER", "SUMMER", "A/S"])
-LaunchScope = streamlit.sidebar.selectbox("LaunchScope", ["WW", "Local"])
-ProcessType = streamlit.sidebar.selectbox("ProcessType", ["MANU", "C3M"])
-MoldTechno = streamlit.sidebar.selectbox("MoldTechno", ["EI", "C3M", "PA/EB/TR"])
-TdGMain_Loop1 = streamlit.sidebar.number_input("TdG Loop1", min_value=0, max_value=10, value=1)
-TdGMain_Loop2 = streamlit.sidebar.number_input("TdG Loop2", min_value=0, max_value=10, value=1)
-Declis = streamlit.sidebar.number_input("Nbr of Indus", min_value=0, max_value=200, value=10)
-CAIs_pourcents = streamlit.sidebar.number_input("% of CAIs at the LC", min_value=0, max_value=100, value=100)
-is_full_regulatory_test = streamlit.sidebar.checkbox("Regulatory tests include DOT or CCC", True)
-is_labelling_mandatory = streamlit.sidebar.checkbox("Include 4 month constraint for Labelling", True)
-Mold_entries_per_week_in_G2LC = streamlit.sidebar.number_input("Mold_entries_per_week_in_G2LC", min_value=1.0, max_value=5.0, value=1., step=0.1)
+
+CI_type = streamlit.sidebar.selectbox("CI Type", ["Proportions", "Others: TBD"])
+
+if CI_type=="Proportions":
+    n = streamlit.sidebar.number_input("N (size)", min_value=0, max_value=10000000, value=10)
+    r = streamlit.sidebar.number_input("r (observed occurences)", min_value=0, max_value=n, value=0)
+    z_dic = {"90%":1.645, "95%":1.960, "99%":2.576}
+    confidence_level = streamlit.sidebar.selectbox("Confidence level", z_dic.keys())    
+    z = z_dic[confidence_level]
+    cilb, ciub = get_proportion_bounds(n, r, z)
+
+    streamlit.write(f"for n={n} and r={r}")
+    streamlit.write("measured proportion is %.1f%%" % (float(r/n)*100))
+    streamlit.write("CI is [%.1f%%  , %.1f%%]" % (cilb*100, ciup*100))
+
 
